effd3a69f568ce6375003357c8c301c5571eff91
mishaseniv <mykhailo.seniv@gmail.com>
2015-08-07 15:00:17 +0300

    add facebook controller

---
+++ app/js/app.js
@@ -0,0 +1,55 @@
+angular.module("app", ['ui.router'])
+.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+  $stateProvider
+  .state('base', {
+    abstract: true,
+    url: '',
+    views: {
+      '@': {
+        templateUrl: 'templates/common/layout.html'
+      },
+      'header@base': {
+        templateUrl: 'templates/common/header.html'
+      },
+      'leftbar@base': {
+        templateUrl: 'templates/common/leftbar.html'
+      },
+      'footer@base': {
+        templateUrl: 'templates/common/footer.html'
+      }
+    }
+  })
+  .state('base.index', {
+    url: '',
+    views: {
+      'content@base': {
+        templateUrl: 'templates/page.html'
+      }
+    }
+  })
+  .state('base.about', {
+    url: '/about',
+    views: {
+      'content@base': {
+        templateUrl: 'templates/about.html',
+        controller: 'AboutController'
+      }
+    }
+  });
+}]);
+
+window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '1602241300037341',
+      xfbml      : true,
+      version    : 'v2.4'
+    });
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
